f015f1e9242892f5c252941978015f4afa5eab4e
maddireddyvenkatesh24 <74550668+maddireddyvenkatesh24@users.noreply.github.com>
2020-11-19 17:46:29 +0530

    Create lwcIterator.js

---
+++ Module 03/LWC Iterator Foreach/lwcIterator.js
@@ -0,0 +1,27 @@
+mport { LightningElement } from 'lwc';
+
+export default class IteratorDemo extends LightningElement {
+
+    contacts = [
+        {
+            Id: 'C1',
+            Name: 'venkatesh',
+            Email: 'test1@gmail.com',
+        },
+        {
+            Id: 'C2',
+            Name: 'ashok',
+            Email: 'test2@gmail.com',
+        },
+        {
+            Id: 'C3',
+            Name: 'balakrishna',
+            Email: 'test3@gmail.com',
+        },
+        {
+            Id: 'C4',
+            Name: 'nagarjuna',
+            Email: 'test4@gmail.com',
+        }
+    ];
+}
